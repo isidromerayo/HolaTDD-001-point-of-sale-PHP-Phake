662dbf753250497b9e0ff71f90a761968b40fb87
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2011-08-22 17:44:50 +0200

    Initial infraestructure

---
+++ tests/TestHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+define('BASE_PATH', __DIR__ . '/../');
+define('APPLICATION_PATH', BASE_PATH . '/src');
+define('TEST_PATH', BASE_PATH . '/tests');
+define('APPLICATION_ENV', 'testing');
+
+// Include path
+set_include_path(
+    '.'
+    . PATH_SEPARATOR . BASE_PATH . '/src'
+    . PATH_SEPARATOR . get_include_path()
+);
+
+// Set the default timezone !!!
+date_default_timezone_set('Europe/Madrid');
+
+// We wanna catch all errors en strict warnings
+error_reporting(E_ALL|E_STRICT);
+
+
+spl_autoload_register(function($class)
+{
+    $file = __DIR__.'/../src/'.strtr($class, '\\', '/').'.php';
+    if (file_exists($file)) {
+        require $file;
+        return true;
+    }
+});
